a8ceac97ffa1a062d521a46a3a22d0cf6e5adf31
André Vitor <andrevitor103@gmail.com>
2021-11-11 14:03:09 -0300

    Adição do campo de ingredientes nos lanches

--- app-food-time/src/components/SnackList/index.tsx
+++ app-food-time/src/components/SnackList/index.tsx
@@ -5,10 +5,30 @@ import { Snack } from "../Snack";
 function SnackList() {
   return (
     <div className={styles.SnackListWrapper}>
-      <Snack image="https://picsum.photos/200" name="X salada" price={12.2} />
-      <Snack image="https://picsum.photos/200" name="X salada" price={12.2} />
-      <Snack image="https://picsum.photos/200" name="X salada" price={12.2} />
-      <Snack image="https://picsum.photos/200" name="X salada" price={12.2} />
+      <Snack
+        image="https://picsum.photos/200"
+        name="X salada"
+        price={12.2}
+        ingredients="Pão, hamburguer, salada e queijo"
+      />
+      <Snack
+        image="https://picsum.photos/200"
+        name="X salada"
+        price={12.2}
+        ingredients="Pão, hamburguer, salada e queijo"
+      />
+      <Snack
+        image="https://picsum.photos/200"
+        name="X salada"
+        price={12.2}
+        ingredients="Pão, hamburguer, salada e queijo"
+      />
+      <Snack
+        image="https://picsum.photos/200"
+        name="X salada"
+        price={12.2}
+        ingredients="Pão, hamburguer, salada e queijo"
+      />
     </div>
   );
 }
