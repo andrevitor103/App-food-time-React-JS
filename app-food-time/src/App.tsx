88258b55e1f36a05b91742a76e281f3d8f04c6f9
André Vitor <andrevitor103@gmail.com>
2021-11-11 14:45:39 -0300

    Criação da lista de comentarios

--- app-food-time/src/App.tsx
+++ app-food-time/src/App.tsx
@@ -1,6 +1,7 @@
 import styles from "./AppMain.module.scss";
 import { Comment } from "./components/Comments";
 import { Header } from "./components/Header";
+import { ListComments } from "./components/ListComments";
 import { SnackList } from "./components/SnackList";
 
 function App() {
@@ -8,12 +9,7 @@ function App() {
     <div style={{ backgroundColor: "#121212" }}>
       <Header />
       <SnackList />
-      <Comment
-        message="melhor atendimento da cidade"
-        star={4}
-        username="andrevitor103"
-        userImage="https://picsum.photos/200"
-      />
+      <ListComments />
     </div>
   );
 }
